1a2df82967509acfcabd71c2f2686b212b7fb43d
jeferson-santos <jefersonsantos.prog@gmail.com>
2022-09-29 22:01:42 -0300

    First Commit

---
+++ __init__.py
@@ -0,0 +1,129 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+app.secret_key = 'many random bytes'
+
+app.config['MYSQL_HOST'] = '35.198.55.42'
+app.config['MYSQL_USER'] = 'netbr'
+app.config['MYSQL_PASSWORD'] = 'netbr'
+app.config['MYSQL_DB'] = 'RH'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def Index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM users")
+    data = cur.fetchall()
+    cur.close()
+
+    return render_template('index.html', users=data )
+
+@app.route('/admin')
+def admin():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT  * FROM users")
+    data = cur.fetchall()
+    cur.close()
+
+    return render_template('admin.html', users=data )
+
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == "POST":
+        flash("Data Inserted Successfully")
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        phone = request.form['phone']
+        password = request.form['password']
+        profile = request.form['profile']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (firstname, lastname, phone, email, pass, profile) VALUES (%s, %s, %s, %s, %s, %s)", (firstname, lastname, phone, email, password, profile))
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
+
+
+@app.route('/delete/<string:id_data>', methods = ['GET'])
+def delete(id_data):
+    print(id_data)
+    flash("Record Has Been Deleted Successfully")
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM users WHERE email=%s", (id_data,))
+    mysql.connection.commit()
+    return redirect(url_for('Index'))
+
+
+
+@app.route('/update',methods=['POST','GET'])
+def update():
+
+    if request.method == 'POST':
+        email = request.form['id']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        phone = request.form['phone']
+        password = request.form['password']
+        profile =  request.form['profile']
+
+        print("EMAILLLL")
+        print(email)
+
+        cur = mysql.connection.cursor()
+        cur.execute("""
+               UPDATE users
+               SET firstname=%s, lastname=%s, email=%s, phone=%s, pass=%s, profile=%s
+               WHERE email=%s
+            """, (firstname, lastname, email, phone, password, profile, email))
+        flash("Data Updated Successfully")
+        mysql.connection.commit()
+        return redirect(url_for('admin'))
+
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+
+        # Get users
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT email, pass, profile FROM users")
+        data = cur.fetchall()
+        cur.close()
+        
+        # Users looping
+        for user in data:
+            email = user[0]
+            password  = user[1]
+            profile = user[2]
+    
+            # Verify email
+            if request.form['email'] == email:
+                print("Encontrou o email")
+
+                # Verify password
+                if request.form['password'] == password:
+                    print("Senha está correta!")
+
+                    # Verify profile ADMIN
+                    if profile == 1:
+                        return redirect(url_for('admin'))
+        
+                    return redirect(url_for('Index'))
+
+        error = 'Invalid Credentials. Please try again.'
+
+    return render_template('login.html', error=error)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ crud.sql
@@ -0,0 +1,58 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `groups` (
+  `id` int NOT NULL,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (1,'ADMIN'),(2,'USER'),(3,'VIEW');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `firstname` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `phone` varchar(200) DEFAULT NULL,
+  `email` varchar(200) NOT NULL,
+  `pass` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `profile` int NOT NULL,
+  PRIMARY KEY (`email`),
+  KEY `fk_group_id` (`profile`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('Jeferson','Santos','(48) 991683421','jeferson.santos@netbr.com.br','teste123',1),('Maria','Antonia','(48) 991683421','maria.antonia@netbr.com.br','teste',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-29 21:59:15
